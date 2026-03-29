72c0f4cc4eba45ca9f72c50728af74565d206bf1
DhimazRafif <hanafidhimaz@gmail.com>
2026-03-29 15:58:04 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_103082430003/Program.cs
+++ tpmodul5_103082430003/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T Item)
+    {
+        Console.WriteLine($"Halo User {Item}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //SapaUser
+        HaloGeneric hg = new HaloGeneric();
+        string User = "Dhimaz Rafif Hanafi";
+        hg.SapaUser(User);
+    }
+}
