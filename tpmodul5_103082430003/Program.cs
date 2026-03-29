d0084d7e5cbeaed4ffa6f8b3653593fede6671e8
DhimazRafif <hanafidhimaz@gmail.com>
2026-03-29 16:19:04 +0700

    Menambahkan class DataGeneric

--- tpmodul5_103082430003/Program.cs
+++ tpmodul5_103082430003/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T Item)
+    {
+        this.data = Item;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah : {data}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataGeneric<string> nim = new DataGeneric<string>("103082430003");
+        nim.PrintData();
+    }
+}
